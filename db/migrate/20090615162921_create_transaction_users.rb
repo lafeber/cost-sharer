13b5c05c2a0ee0ec26f92100af73c522f8d59eb0
Lukas Spee <lukasspee@innovationfactory.eu>
2009-06-17 20:12:58 +0200

    Fixed initial transaction_user migration

--- db/migrate/20090615162921_create_transaction_users.rb
+++ db/migrate/20090615162921_create_transaction_users.rb
@@ -1,14 +1,14 @@
 class CreateTransactionUsers < ActiveRecord::Migration
   def self.up
     create_table :transaction_users do |t|
-      t.integer :transction_id
+      t.integer :transaction_id
       t.integer :user_id
       t.integer :count, :default => 1
       t.timestamps
     end
     
-    add_index :transaction_id
-    add_index :user_id
+    add_index :transaction_users, :transaction_id
+    add_index :transaction_users, :user_id
   end
 
   def self.down
