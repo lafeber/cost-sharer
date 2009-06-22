4697fd9e59174096c6a8a10f9df1d30880ccdd0b
Martijn Lafeber <lafeber@gmail.com>
2009-06-22 15:43:37 +0200

    balance function in groups_users

--- app/models/groups_users.rb
+++ app/models/groups_users.rb
@@ -1,8 +1,13 @@
 class GroupsUsers < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user do
+    def balance
+      transactions.find :all, :conditions => ["transactions.group_id = #{group_id}"]
+    end
+  end
   belongs_to :group
   
-  def total_amount
-    user.transactions.
+  def can_be_removed?
+    
   end
+  
 end
