71966cc4fe31d5157020feb1a56767d51156acab
Martijn Lafeber <lafeber@gmail.com>
2009-06-22 08:58:38 +0200

    removed def with sql from group, added another

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ActiveRecord::Base
   has_and_belongs_to_many :users, :join_table => "groups_users", :foreign_key => "users_id"
+  has_one :admin, :through => :users, :join_table => "groups_users", :conditions => "groups_users.is_admin = true"
   
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,8 @@
 class Transaction < ActiveRecord::Base
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users  
   
-  #TODO rewrite this in nice ruby code?
-  def total_users
-    find_by_sql("SELECT SUM (tu.count) FROM transaction_users tu WHERE tu.transaction_id = #{id}")
+  def amount_per_person
+    amount/numer_of_people
   end
+  
 end
