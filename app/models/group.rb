208e3f6d20525e9e106894b9b4f7ccd3b61221da
Lukas Spee <lukasspee@innovationfactory.eu>
2009-06-28 22:32:05 +0200

    Fixed typo and commented out admin association for now, because this generates an error

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ActiveRecord::Base
   has_and_belongs_to_many :users
-  has_one :admin, :through => :users, :join_table => "groups_users", :conditions => "groups_users.is_admin = true"
+  #has_one :admin, :through => :users, :join_table => "groups_users", :conditions => "groups_users.is_admin = true"
   has_many :transactions
-
 end
