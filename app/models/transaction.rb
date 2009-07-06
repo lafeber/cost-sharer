6ed1d83ced4df1bd4910375019eaf0c6a9d7f817
Martijn Lafeber <lafeber@gmail.com>
2009-07-06 13:55:39 +0200

    removed bs from transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,10 +1,2 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :group
-  has_many :transaction_users
-  has_and_belongs_to_many :users
-  
-  def amount_per_person
-    amount/numer_of_people.to_f
-  end
-  
 end
