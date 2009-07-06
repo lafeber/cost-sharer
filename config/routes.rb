9df262f0fc73dc8607e9b56446c8de7d18900a35
Martijn Lafeber <lafeber@gmail.com>
2009-07-06 13:56:35 +0200

    transactions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :transactions
+
   map.resources :groups
   map.connect '/:name/', :controller => 'groups', :action => 'show'
   map.connect '/:name/edit', :controller => 'groups', :action => 'edit'
