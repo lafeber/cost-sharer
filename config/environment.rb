75639cc9228f96fbd9fea5892515f8bcaf17e9b3
Martijn Lafeber <lafeber@gmail.com>
2009-07-06 13:56:11 +0200

    added factory girl in ENV

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,10 @@
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem "thoughtbot-factory_girl",
+             :lib    => "factory_girl",
+             :source => "http://gems.github.com"
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
