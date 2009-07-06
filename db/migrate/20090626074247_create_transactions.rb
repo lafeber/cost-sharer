fc9c2408db317e58ad5b5707b5dc71480119b95b
Martijn Lafeber <lafeber@gmail.com>
2009-07-06 13:55:15 +0200

    renamed transactions migration

--- db/migrate/20090626074247_create_transactions.rb
+++ db/migrate/20090626074247_create_transactions.rb
@@ -1,10 +1,6 @@
 class CreateTransactions < ActiveRecord::Migration
   def self.up
     create_table :transactions do |t|
-      t.integer :group_id
-      t.float :amount
-      t.string :name
-      t.datetime :paid_at
 
       t.timestamps
     end
