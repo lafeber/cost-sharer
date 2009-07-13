a449a18fd92eeb475cbfbd9cc5dfe8c400e3cd49
Martijn Lafeber <lafeber@gmail.com>
2009-07-13 15:02:14 +0200

    a helper for euro currency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def euro(amount)
+    number_to_currency(amount, :unit => '&euro; ', :separator => ",", :delimiter => "" )
+  end
 end
