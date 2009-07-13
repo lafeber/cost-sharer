e303dab6a3e9352d4edfc183e0a0940484b19538
Martijn Lafeber <lafeber@gmail.com>
2009-07-13 15:02:34 +0200

    removed commented out shizzle

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,4 @@
-class Group < ActiveRecord::Base
-  # has_and_belongs_to_many :users
-  # has_many :users, :through => "members", :foreign_key => "group_id"
-  
+class Group < ActiveRecord::Base  
   has_and_belongs_to_many :users, :join_table => "members", :foreign_key => "group_id"
   has_many :transactions
 end
