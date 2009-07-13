91e709f82e671477bdae683d8b6ad8ae9ffbe53c
Martijn Lafeber <lafeber@gmail.com>
2009-07-13 15:01:54 +0200

    groups nested in transactions in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :transactions
   map.resources :users
-  
   map.resource :session
   
-  map.resources :groups
-  map.root :controller => "sessions", :action => 'new'
+  map.resources :groups do |group|
+    group.resources :transactions
+  end
+  
+  map.root :controller => "groups"
 
   map.signup  '/signup', :controller => 'users',   :action => 'new'
   map.login  '/login',  :controller => 'sessions', :action => 'new'
