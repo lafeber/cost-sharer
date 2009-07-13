56ab3f6d0cc9bed287addbeffa08e02aa473fb72
Martijn Lafeber <lafeber@gmail.com>
2009-07-13 14:10:08 +0200

    translations for mail

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,23 +1,23 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
+    @subject    += 'Activeer je account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://www.kostendeler.nl/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
+    @subject    += 'Je account is geactiveerd!'
     @body[:url]  = "http://YOURSITE/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.login}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "kostendeler@coupling.nl"
+      @subject     = "kostendeler.nl "
       @sent_on     = Time.now
       @body[:user] = user
     end
